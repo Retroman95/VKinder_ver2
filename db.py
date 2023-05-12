201a4be220396ee90f4d84e2f1f562be7e757d0a
Retroman95 <46816641+Retroman95@users.noreply.github.com>
2023-05-12 16:58:21 +0400

    Update db.py

--- db.py
+++ db.py
@@ -6,83 +6,44 @@ with psycopg2.connect(user=user, password=password, database=database) as conn:
     conn.autocommit = True
 
 # создаем таблицу
-def create_table_users():
-    with conn.cursor() as cursor:
-        cursor.execute(
-            """CREATE TABLE IF NOT EXISTS seen_users(
-            id SERIAL,            vk_id INT PRIMARY KEY);
-            """)
+# def create_table_users():
+#     with conn.cursor() as cursor:
+#         cursor.execute(
+#             """CREATE TABLE IF NOT EXISTS seen_users(
+#             id SERIAL,            vk_id INT PRIMARY KEY);
+#             """)
 
 # добавляем id найденных пользователей
-def insert_data_search(vk_ids):
+def insert_data_search(new_vk_ids):
     with conn.cursor() as cursor:
-        # исключаем повторяющихся пользователей в списке найденных пользователей
-        new_vk_ids = exclude_duplicates(vk_ids)
-        print("new_vk_ids:", new_vk_ids)
-        if not new_vk_ids:
-            return
-        try:
-            # формируем SQL-запрос, чтобы получить список уже существующих пользователей
-            select_query = "SELECT vk_id FROM seen_users WHERE vk_id IN %s"
-            placeholders_list = ["%s" for _ in range(len(new_vk_ids))]
-            placeholders = ", ".join(placeholders_list)
-            select_query = select_query % "(" + placeholders + ")"
-            # передаем список значений в метод execute()
-            cursor.execute(select_query, tuple(new_vk_ids))
-            existing_vk_ids = [result[0] for result in cursor.fetchall()]
-            # создаем список новых пользователей, которых еще нет в базе данных
-            new_vk_ids = list(set(new_vk_ids) - set(existing_vk_ids))
-            print("new_vk_ids (after checking against the database):", new_vk_ids)
-            # добавляем новых пользователей в базу данных
-            if new_vk_ids:
-                # формируем SQL-запрос с помощью строки форматирования
-                insert_query = "INSERT INTO seen_users (vk_id) VALUES %s"
-                placeholders_list = ["%s" for _ in range(len(new_vk_ids))]
-                placeholders = ", ".join(placeholders_list)
-                insert_query = insert_query % "(" + placeholders + ")"
-                # передаем список значений в метод execute()
-                values = [str(vk_id) for vk_id in new_vk_ids]
-                cursor.execute(insert_query, tuple(values))
-                conn.commit()
-        except IntegrityError as e:
-            if e.pgcode == errorcodes.UNIQUE_VIOLATION:
-                # ignore the error, the user is already in the database
-                pass
-            else:
-                raise e
+        delete_query = "DELETE FROM temp_results"
+        cursor.execute(delete_query)
+        insert_query = "INSERT INTO temp_results(vk_id) VALUES (%s)"
+        cursor.executemany(insert_query, [(vk_id,) for vk_id in new_vk_ids])
+        select_query = "SELECT DISTINCT vk_id FROM temp_results WHERE vk_id NOT IN (SELECT vk_id FROM seen_users)"
+        cursor.execute(select_query)
+        unique_vk_ids = [result[0] for result in cursor.fetchall()]
+        insert_query = "INSERT INTO seen_users(vk_id) VALUES (%s)"
+        cursor.executemany(insert_query, [(vk_id,) for vk_id in unique_vk_ids])
 
-
-
-
-# исключаем повторяющихся пользователей в списке найденных пользователей
-def exclude_duplicates(vk_ids):
-    if not vk_ids:
-        return []
-    with conn.cursor() as cursor:
-        sql = "SELECT vk_id FROM seen_users WHERE vk_id IN (%s)"
-        placeholders = ", ".join("%s" for _ in vk_ids)
-        sql = sql % placeholders
-        values = [(vk_id,) for vk_id in vk_ids]
-        cursor.execute(sql, values)
-        seen_users = cursor.fetchall()
-        new_vk_ids = [vk_id for vk_id in vk_ids if str(vk_id) not in seen_users]
-        return new_vk_ids
 
 # запрашиваем список id найденных пользователей
 def get_seen_users():
     with conn.cursor() as cursor:
-        cursor.execute(
-            """SELECT vk_id FROM seen_users;"""
-        )
+        cursor.execute("""
+            SELECT DISTINCT sr.vk_id
+            FROM seen_users su
+            JOIN temp_results sr ON su.vk_id = sr.vk_id
+        """)
         seen_users = cursor.fetchall()
         return seen_users
 
-# удаляем таблицу
-def drop_users():
-    with conn.cursor() as cursor:
-        cursor.execute(
-            """DROP TABLE IF EXISTS seen_users CASCADE;"""
-        )
+# # удаляем таблицу
+# def drop_users():
+#     with conn.cursor() as cursor:
+#         cursor.execute(
+#             """DROP TABLE IF EXISTS seen_users CASCADE;"""
+#         )
 
 def create_database():
     with conn.cursor() as cursor:
@@ -90,17 +51,31 @@ def create_database():
         cursor.execute("SELECT EXISTS(SELECT * FROM information_schema.tables WHERE table_name='seen_users')")
         table_exists = cursor.fetchone()[0]
         if table_exists:
-            print("Table seen_users already exists")
+            print("Table search_results already exists")
         else:
-            # создаем таблицу seen_users, если она не существует
+            # создаем таблицу search_results, если она не существует
             cursor.execute("""
                 CREATE TABLE seen_users (
-                    id SERIAL PRIMARY KEY,
-                    vk_id BIGINT UNIQUE NOT NULL
+                    vk_id BIGINT PRIMARY KEY
                 )
             """)
             conn.commit()
-            print("Table seen_users created")
+            print("Table search_results created")
+
+        # проверяем, существует ли таблица temp_results в базе данных
+        cursor.execute("SELECT EXISTS(SELECT * FROM information_schema.tables WHERE table_name='temp_results')")
+        table_exists = cursor.fetchone()[0]
+        if table_exists:
+            print("Table temp_results already exists")
+        else:
+            # создаем таблицу temp_results, если она не существует
+            cursor.execute("""
+                CREATE TABLE temp_results (
+                    vk_id BIGINT PRIMARY KEY
+                )
+            """)
+            conn.commit()
+            print("Table temp_results created")
 
 if __name__ == '__main__':
     create_database()
